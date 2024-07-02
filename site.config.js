79fe96d1dc6f73f1947faf0cf7c1bc2a2f845558
imkilwon <yuvwz_coding@naver.com>
2024-07-02 15:17:50 +0900

    사전 설정

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    name: "Jeon kilwon",
+    image: "public\profile.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Data Analysis",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    email: "yuvw123@gmail.com",
+    linkedin: "전길원 (Jeon Kilwon)",
+    github: "kingrangE",
+    instagram: "1w0o1n4",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `FinSight(금융데이터 기반 매수/매도 시점 추천 웹서비스)`,
+      href: "https://github.com/kingrangE/FinSight",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "won-log",
+    description: "welcome to won-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
@@ -54,7 +54,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
